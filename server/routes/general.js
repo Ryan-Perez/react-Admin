9cdb04209240b13abe532d2d84bb6e19d15a1e0b
Ryan-Perez <perezryanm@gmail.com>
2022-12-09 12:03:11 -0800

    created server side

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { getUser, getDashboardStats } from "../controllers/general.js";
 
 const router = express.Router();
 
-export default router
+router.get("/user/:id", getUser);
+router.get("/dashboard", getDashboardStats);
+
+export default router;
